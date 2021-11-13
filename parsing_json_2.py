14b95e2c500318565e5f553c85c3e0f3bd666821
desvan <esvan@yandex.ru>
2021-11-13 19:15:15 +0300

    commit 2.1

---
+++ parsing_json_2.py
@@ -0,0 +1,8 @@
+import json
+
+
+json_text = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And ' \
+            'this is a second message","timestamp":"2021-06-04 16:41:01"}]} '
+obj = json.loads(json_text)
+
+print(obj['messages'][1]['message'])
