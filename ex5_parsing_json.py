205cd03e41b892da0a7bee303fb87a982dbabbb0
desvan <esvan@yandex.ru>
2021-11-13 23:56:45 +0300

    ex5 and ex6 optimization

--- ex5_parsing_json.py
+++ ex5_parsing_json.py
@@ -5,11 +5,11 @@ json_text = '{"messages":[{"message":"This is the first message","timestamp":"20
             'this is a second message","timestamp":"2021-06-04 16:41:01"}]} '
 obj = json.loads(json_text)
 
-key = "message"
+key = "message2"
 
 try:
     print(obj["messages"][1][key])
 except KeyError:
-    print(f'There is no key parameter <{key}> in JSON or an incorrect path is specified for the key parameter <{key}>')
+    print(f"There is no key parameter <{key}> in JSON or an incorrect path is specified for the key parameter <{key}>")
 
 
