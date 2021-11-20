53ff6c015193823b27466224e985b261ee475650
desvan <esvan@yandex.ru>
2021-11-21 01:26:21 +0300

    ex11 and ex12 reFucktoring)

--- ex11_cookie_check.py
+++ ex11_cookie_check.py
@@ -3,12 +3,11 @@ import requests
 
 test_url = "https://playground.learnqa.ru/api/homework_cookie"
 
-# expected cookie {'HomeWork': 'hw_value'}
 class TestCookie:
     def test_cookie(self):
+        print("Expected cookie is {'HomeWork': 'hw_value'}")
         actual_response = requests.get(test_url)
         actual_cookie = dict(actual_response.cookies)
-        print(actual_cookie)
         assert "HomeWork" in actual_cookie, f"Expected cookie key 'HomeWork' is missing in the response"
-        assert actual_cookie["HomeWork"] == "hw_value", f"Expected cookie value is 'hw_value'," \
-                                                        f" actual cookie value is {actual_cookie['HomeWork']}"
+        assert actual_cookie["HomeWork"] == "hw_value", f"Expected cookie value: 'hw_value'," \
+                                                        f" actual cookie value: {actual_cookie['HomeWork']}"
---
+++ ex12_header_check.py
@@ -0,0 +1,14 @@
+import requests
+
+
+test_url = "https://playground.learnqa.ru/api/homework_header"
+
+class TestHeader:
+    def test_header(self):
+        print("Expected header is {'x-secret-homework-header': 'Some secret value'}")
+        actual_response = requests.get(test_url)
+        actual_header = dict(actual_response.headers)
+        assert "x-secret-homework-header" in actual_header, f"Expected header key 'x-secret-homework-header' " \
+                                                            f"is missing in the response"
+        assert actual_header["x-secret-homework-header"] == "Some secret value",\
+            f"Expected cookie value: 'Some secret value', actual header value: {actual_header['x-secret-homework-header']}"
