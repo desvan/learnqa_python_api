392ba61b5c474b5548e4e71a78cdc2d4678272f5
desvan <esvan@yandex.ru>
2021-11-21 12:15:10 +0300

    ex11 and ex12 added assert wrong response code

--- ex11_cookie_check.py
+++ ex11_cookie_check.py
@@ -8,6 +8,7 @@ class TestCookie:
         print("Expected cookie is {'HomeWork': 'hw_value'}")
         actual_response = requests.get(test_url)
         actual_cookie = dict(actual_response.cookies)
+        assert actual_response.status_code == 200, 'Wrong response code'
         assert "HomeWork" in actual_cookie, f"Expected cookie key 'HomeWork' is missing in the response"
         assert actual_cookie["HomeWork"] == "hw_value", f"Expected cookie value: 'hw_value'," \
                                                         f" actual cookie value: {actual_cookie['HomeWork']}"
--- ex12_header_check.py
+++ ex12_header_check.py
@@ -1,14 +1,15 @@
 import requests
 
-
 test_url = "https://playground.learnqa.ru/api/homework_header"
+
 
 class TestHeader:
     def test_header(self):
         print("Expected header is {'x-secret-homework-header': 'Some secret value'}")
         actual_response = requests.get(test_url)
         actual_header = dict(actual_response.headers)
+        assert actual_response.status_code == 200, 'Wrong response code'
         assert "x-secret-homework-header" in actual_header, f"Expected header key 'x-secret-homework-header' " \
                                                             f"is missing in the response"
-        assert actual_header["x-secret-homework-header"] == "Some secret value",\
+        assert actual_header["x-secret-homework-header"] == "Some secret value", \
             f"Expected cookie value: 'Some secret value', actual header value: {actual_header['x-secret-homework-header']}"
